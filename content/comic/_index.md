be998a7cea2c04521c68b0b950d53858782706df
Benjamin Neff <benjamin@coding4coffee.ch>
2021-01-25 01:43:38 +0100

    Use /archives like on real site

---
+++ content/comic/_index.md
@@ -0,0 +1,4 @@
+---
+title: Comic Archive
+url: /archives/
+---
--- static/index.php
+++ static/index.php
@@ -1,5 +1,5 @@
 <?php
-$json = file_get_contents(__DIR__ . '/comic/index.json');
+$json = file_get_contents(__DIR__ . '/archives/index.json');
 $comics = json_decode($json, true);
 $comic = $_SERVER["REQUEST_URI"] == "/random" ? $comics[array_rand($comics)] : $comics[0];
 header('Location: /comic/' . $comic . '/');
