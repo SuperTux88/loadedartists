7c1302240785c9b7df995ed4a7cb61ac7f7893a9
Benjamin Neff <benjamin@coding4coffee.ch>
2021-02-14 16:04:33 +0100

    Exclude referer in /random

--- static/index.php
+++ static/index.php
@@ -1,6 +1,20 @@
 <?php
 $json = file_get_contents(__DIR__ . '/archives/index.json');
 $comics = json_decode($json, true);
-$comic = $_SERVER["REQUEST_URI"] == "/random" ? $comics[array_rand($comics)] : $comics[0];
+
+if ($_SERVER["REQUEST_URI"] === "/random") {
+  $referer = $_SERVER["HTTP_REFERER"];
+  if (parse_url($referer, PHP_URL_HOST) === $_SERVER["HTTP_HOST"]) {
+    if (preg_match('/\\/comic\\/([^\\/]+)/', parse_url($referer, PHP_URL_PATH), $matches) === 1) {
+      if (($del_key = array_search($matches[1], $comics)) !== false) {
+        unset($comics[$del_key]);
+      }
+    }
+  }
+  $comic = $comics[array_rand($comics)];
+} else {
+  $comic = $comics[0];
+}
+
 header('Location: /comic/' . $comic . '/');
 ?>
